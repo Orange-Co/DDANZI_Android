22083e6748adbabd2a2c53d1de8f867ff1fdc4b4
Sangho Kim <marchbreeze03@gmail.com>
2024-06-12 03:22:28 +0900

    [ADD/#5] 메인뷰 기본 뷰 생성

---
+++ presentation/src/main/java/co/orange/presentation/main/MainActivity.kt
@@ -0,0 +1,54 @@
+package co.orange.presentation.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import co.orange.presentation.main.home.HomeFragment
+import co.orange.presentation.main.profile.ProfileFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBnvItemIconTintList()
+        initBnvItemSelectedListener()
+    }
+
+    fun initBnvItemIconTintList() {
+        with(binding.bnvMain) {
+            itemIconTintList = null
+            selectedItemId = R.id.menu_feed
+            labelVisibilityMode = NavigationBarView.LABEL_VISIBILITY_UNLABELED
+        }
+    }
+
+    private fun initBnvItemSelectedListener() {
+        supportFragmentManager.findFragmentById(R.id.fcv_main) ?: navigateTo<HomeFragment>()
+
+        binding.bnvMain.setOnItemSelectedListener { menu ->
+            if (binding.bnvMain.selectedItemId == menu.itemId) {
+                return@setOnItemSelectedListener false
+            }
+            when (menu.itemId) {
+                R.id.menu_feed -> navigateTo<HomeFragment>()
+
+                R.id.menu_create -> navigateTo<ProfileFragment>()
+
+                else -> return@setOnItemSelectedListener false
+            }
+            true
+        }
+    }
+
+    private inline fun <reified T : Fragment> navigateTo() {
+        supportFragmentManager.commit {
+            replace<T>(R.id.fcv_main, T::class.java.canonicalName)
+        }
+    }
+}

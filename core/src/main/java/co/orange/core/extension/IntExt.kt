bf2f790728263a8140910bcb8d67777c9d4bd49e
Sangho Kim <marchbreeze03@gmail.com>
2024-06-13 03:26:10 +0900

    [ADD/#9] 줄바꿈, 숫자표현 관련 확장함수 설정

--- core/src/main/java/co/orange/core/extension/IntExt.kt
+++ core/src/main/java/co/orange/core/extension/IntExt.kt
@@ -2,8 +2,14 @@ package kr.genti.core.extension
 
 import android.content.Context
 import android.util.TypedValue
+import java.text.DecimalFormat
 
 fun Int.dpToPx(context: Context): Int {
     val metrics = context.resources.displayMetrics
     return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this.toFloat(), metrics).toInt()
+}
+
+fun Int.setNumberForm() : String {
+    val decimal = DecimalFormat("#,###")
+    return decimal.format(this)
 }

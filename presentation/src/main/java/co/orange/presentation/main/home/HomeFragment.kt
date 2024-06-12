aa1b7edd5186568af4acae17f1742509d42cd5a7
Sangho Kim <marchbreeze03@gmail.com>
2024-06-12 14:00:20 +0900

    [FEAT/#7] 홈뷰 멀티뷰타입 구현

--- presentation/src/main/java/co/orange/presentation/main/home/HomeFragment.kt
+++ presentation/src/main/java/co/orange/presentation/main/home/HomeFragment.kt
@@ -2,13 +2,22 @@ package co.orange.presentation.main.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import co.orange.presentation.main.home.HomeAdapter.Companion.VIEW_TYPE_BANNER
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseFragment
+import kr.genti.core.extension.initOnBackPressedListener
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.FragmentHomeBinding
 
 @AndroidEntryPoint
 class HomeFragment() : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private var _adapter: HomeAdapter? = null
+    val adapter
+        get() = requireNotNull(_adapter) { getString(R.string.adapter_not_initialized_error_msg) }
+
+    private val viewModel by activityViewModels<HomeViewModel>()
 
     override fun onViewCreated(
         view: View,
@@ -16,5 +25,51 @@ class HomeFragment() : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home)
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        initView()
+        initAdapter()
+        setGridRecyclerView()
+        setItemList()
+    }
+
+    private fun initView() {
+        initOnBackPressedListener(binding.root)
+    }
+
+    private fun initAdapter() {
+        _adapter =
+            HomeAdapter(
+                bannerClick = ::initBannerClickListener,
+                productClick = ::initProductClickListener,
+                likeClick = ::initLikeClickListener
+
+            )
+        binding.rvHome.adapter = adapter
+    }
+
+    //TODO: 버튼 리스너 설정
+    private fun initBannerClickListener(unit: Unit) {}
+    private fun initProductClickListener(unit: Unit) {}
+    private fun initLikeClickListener(unit: Unit) {}
+
+    private fun setGridRecyclerView() {
+        binding.rvHome.layoutManager = GridLayoutManager(context, 2).apply {
+            spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return when (adapter.getItemViewType(position)) {
+                        VIEW_TYPE_BANNER -> 2
+                        else -> 1
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setItemList() {
+        adapter.setItemList(viewModel.mockItemList)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _adapter = null
     }
 }

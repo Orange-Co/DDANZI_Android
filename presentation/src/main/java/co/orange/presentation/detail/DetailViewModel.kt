764d0a3a84d2880d65445a1e251260cd68d76816
Sangho Kim <marchbreeze03@gmail.com>
2024-06-13 23:47:27 +0900

    [ADD/#9] 옵션 바텀시트 둥근 모서리 설정

--- presentation/src/main/java/co/orange/presentation/detail/DetailViewModel.kt
+++ presentation/src/main/java/co/orange/presentation/detail/DetailViewModel.kt
@@ -13,7 +13,7 @@ constructor(
     // private val feedRepository: FeedRepository,
 ) : ViewModel() {
     val mockProduct = ProductDetailModel(
-        "퓨어 오일 퍼퓸 10ml 입니다 긴제목은 어떻게 될까요? 짧은 것도 이간격",
+        "퓨어 오일 퍼퓸 10 ml 긴제목테스트트트트트",
         "카테고리",
         false,
         true,

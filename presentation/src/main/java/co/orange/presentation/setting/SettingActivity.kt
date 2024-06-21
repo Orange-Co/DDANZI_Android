635fde2e7c4e4971023d28e43a6e355b366bad50
Sangho Kim <marchbreeze03@gmail.com>
2024-06-22 05:52:49 +0900

    [ADD/#23] 설정뷰 액티비티 구현

--- presentation/src/main/java/co/orange/presentation/setting/SettingActivity.kt
+++ presentation/src/main/java/co/orange/presentation/setting/SettingActivity.kt
@@ -1,2 +1,29 @@
 package co.orange.presentation.setting
 
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.setOnSingleClickListener
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivitySettingBinding
+
+@AndroidEntryPoint
+class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBackBtnListener()
+        initManageBtnsListener()
+    }
+
+    private fun initBackBtnListener() {
+        binding.btnBack.setOnSingleClickListener { finish() }
+    }
+
+    private fun initManageBtnsListener() {
+        // TODO
+        binding.btnManageDelivery.setOnSingleClickListener { }
+        binding.btnManageBank.setOnSingleClickListener { }
+        binding.btnManageAccount.setOnSingleClickListener { }
+    }
+}

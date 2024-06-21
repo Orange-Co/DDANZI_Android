86de00c8040632cae932067bfe6679cbc1a0a0b8
Sangho Kim <marchbreeze03@gmail.com>
2024-06-22 05:18:28 +0900

    [ADD/#23] 프로필뷰 fragment 설정

--- presentation/src/main/java/co/orange/presentation/main/profile/ProfileFragment.kt
+++ presentation/src/main/java/co/orange/presentation/main/profile/ProfileFragment.kt
@@ -2,19 +2,59 @@ package co.orange.presentation.main.profile
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseFragment
+import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.FragmentProfileBinding
 
 @AndroidEntryPoint
 class ProfileFragment() : BaseFragment<FragmentProfileBinding>(R.layout.fragment_profile) {
-
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
 
+        initSettingBtnListener()
+        initLoginBtnListener()
+        initWebBtnsListener()
+        initHistoryBtnsListener()
+        checkIsLogined()
+    }
+
+    private fun initSettingBtnListener() {
+        // TODO
+        binding.btnSetting.setOnSingleClickListener { }
+    }
+
+    private fun initLoginBtnListener() {
+        // TODO
+        binding.btnLogin.setOnSingleClickListener { }
+    }
+
+    private fun initWebBtnsListener() {
+        // TODO
+        binding.btnBenefit.setOnSingleClickListener { }
+        binding.btnCenterQuestion.setOnSingleClickListener { }
+        binding.btnCenterOneOnOne.setOnSingleClickListener { }
+    }
+
+    private fun initHistoryBtnsListener() {
+        // TODO
+        binding.btnHistoryPurchase.setOnSingleClickListener { }
+        binding.btnHistorySell.setOnSingleClickListener { }
+        binding.btnHistoryLike.setOnSingleClickListener { }
+    }
+
+    private fun checkIsLogined() {
+        if (false) {
+            with(binding) {
+                layoutYesLogin.isVisible = true
+                layoutNotLogin.isVisible = false
+                tvProfileInfoName.text = "김상호"
+            }
+        }
     }
 }

60c56b26efc0af3a630a3b856b2600b5fca6b9ed
Sangho Kim <marchbreeze03@gmail.com>
2024-06-23 16:06:26 +0900

    [ADD/#23] 설정 배송지 관리 뷰 activity 설정

--- presentation/src/main/java/co/orange/presentation/setting/delivery/DeliveryActivity.kt
+++ presentation/src/main/java/co/orange/presentation/setting/delivery/DeliveryActivity.kt
@@ -1,2 +1,50 @@
 package co.orange.presentation.setting.delivery
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import co.orange.domain.entity.response.AddressInfoModel
+import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.breakLines
+import kr.genti.core.extension.setOnSingleClickListener
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityDeliveryBinding
+
+class DeliveryActivity : BaseActivity<ActivityDeliveryBinding>(R.layout.activity_delivery) {
+    val viewModel by viewModels<DeliveryViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBackBtnListener()
+        initModBtnListener()
+        initDeleteBtnListener()
+        setDeliveryUi(viewModel.mockAddressModel)
+    }
+
+    private fun initBackBtnListener() {
+        binding.btnBack.setOnSingleClickListener { finish() }
+    }
+
+    private fun initModBtnListener() {
+        // TODO
+        binding.btnDeliveryMod.setOnSingleClickListener { }
+    }
+
+    private fun initDeleteBtnListener() {
+        // TODO
+        binding.btnDeliveryDelete.setOnSingleClickListener { }
+    }
+
+    private fun setDeliveryUi(item: AddressInfoModel) {
+        with(binding) {
+            tvDeliveryName.text = item.recipient
+            tvDeliveryAddress.text =
+                getString(
+                    R.string.address_format,
+                    item.zipCode,
+                    item.address,
+                ).breakLines()
+            tvDeliveryPhone.text = item.phone
+        }
+    }
+}

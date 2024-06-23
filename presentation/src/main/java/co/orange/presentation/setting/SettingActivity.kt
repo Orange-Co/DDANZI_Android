eb5087b6f0df11784e7c5c82925bc0d9bb7de58d
Sangho Kim <marchbreeze03@gmail.com>
2024-06-23 14:39:25 +0900

    [ADD/#23] 설정뷰 data class 추가

--- presentation/src/main/java/co/orange/presentation/setting/SettingActivity.kt
+++ presentation/src/main/java/co/orange/presentation/setting/SettingActivity.kt
@@ -1,6 +1,8 @@
 package co.orange.presentation.setting
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import co.orange.domain.entity.response.SettingModel
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
 import kr.genti.core.extension.setOnSingleClickListener
@@ -9,11 +11,14 @@ import kr.genti.presentation.databinding.ActivitySettingBinding
 
 @AndroidEntryPoint
 class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+    val viewModel by viewModels<SettingViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initBackBtnListener()
         initManageBtnsListener()
+        setSettingInfo(viewModel.mockSettingModel)
     }
 
     private fun initBackBtnListener() {
@@ -25,5 +30,13 @@ class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_s
         binding.btnManageDelivery.setOnSingleClickListener { }
         binding.btnManageBank.setOnSingleClickListener { }
         binding.btnManageAccount.setOnSingleClickListener { }
+    }
+
+    private fun setSettingInfo(item: SettingModel) {
+        with(binding) {
+            tvSettingInfoName.text = item.name
+            tvSettingInfoPhone.text = item.phone
+            tvSettingInfoNickname.text = item.nickname
+        }
     }
 }
--- presentation/src/main/java/co/orange/presentation/setting/SettingViewModel.kt
+++ presentation/src/main/java/co/orange/presentation/setting/SettingViewModel.kt
@@ -1,6 +1,7 @@
 package co.orange.presentation.setting
 
 import androidx.lifecycle.ViewModel
+import co.orange.domain.entity.response.SettingModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,4 +10,11 @@ class SettingViewModel
     @Inject
     constructor(
         // private val feedRepository: FeedRepository,
-    ) : ViewModel()
+    ) : ViewModel() {
+        val mockSettingModel =
+            SettingModel(
+                "김상호",
+                "행복한물개물개물개",
+                "010-1234-5678",
+            )
+    }

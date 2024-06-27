9f79cde06e2363213251cf1b9771abc158485c69
Sangho Kim <marchbreeze03@gmail.com>
2024-06-28 01:00:20 +0900

    [ADD/#27] 판매 푸시뷰 액티비티 구현

---
+++ presentation/src/main/java/co/orange/presentation/sell/push/SellPushActivity.kt
@@ -0,0 +1,59 @@
+package co.orange.presentation.sell.push
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.setOnSingleClickListener
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityPushBinding
+
+@AndroidEntryPoint
+class SellPushActivity : BaseActivity<ActivityPushBinding>(R.layout.activity_push) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: 뒤로가기 방지 추가
+        initExitBtnListener()
+        initAlarmBtnListener()
+    }
+
+    private fun initExitBtnListener() {
+        with(binding) {
+            btnExit.setOnSingleClickListener {
+                navigateToFinishedActivity()
+            }
+            btnLater.setOnSingleClickListener {
+                navigateToFinishedActivity()
+            }
+        }
+    }
+
+    private fun initAlarmBtnListener() {
+        binding.btnAlarm.setOnSingleClickListener {
+            // TODO 푸시알람
+            navigateToFinishedActivity()
+        }
+    }
+
+    private fun navigateToFinishedActivity() {
+//        val productId = intent.getLongExtra(EXTRA_PRODUCT_ID,-1)
+//        BuyFinishedActivity.createIntent(this, productId).apply {
+//            startActivity(this)
+//        }
+    }
+
+    companion object {
+        private const val EXTRA_ITEM_ID = "EXTRA_ITEM_ID"
+
+        @JvmStatic
+        fun createIntent(
+            context: Context,
+            itemId: Long,
+        ): Intent =
+            Intent(context, SellPushActivity::class.java).apply {
+                putExtra(EXTRA_ITEM_ID, itemId)
+            }
+    }
+}

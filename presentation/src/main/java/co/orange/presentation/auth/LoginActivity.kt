99ca832c491b6cf30f8f2e8eaf1118d1f5374fb3
Sangho Kim <marchbreeze03@gmail.com>
2024-07-05 08:20:07 +0900

    [ADD/#33] 소셜 로그인뷰 Activity 구현

---
+++ presentation/src/main/java/co/orange/presentation/auth/LoginActivity.kt
@@ -0,0 +1,29 @@
+package co.orange.presentation.auth
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.setOnSingleClickListener
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityLoginBinding
+
+@AndroidEntryPoint
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    private val viewModel by viewModels<LoginViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initLoginBtnListener()
+    }
+
+    private fun initLoginBtnListener() {
+        binding.btnLoginKakao.setOnSingleClickListener {
+//            Intent(this, MainActivity::class.java).apply {
+//                startActivity(this)
+//            }
+//            finish()
+        }
+    }
+}

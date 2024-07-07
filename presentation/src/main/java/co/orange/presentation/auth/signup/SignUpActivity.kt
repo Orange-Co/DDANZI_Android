62ff4dcb372cbd2f3c03187df3dac24afabf2548
Sangho Kim <marchbreeze03@gmail.com>
2024-07-08 01:36:52 +0900

    [FEAT/#33] 회원가입뷰 자동 포커스 이동 구현

--- presentation/src/main/java/co/orange/presentation/auth/signup/SignUpActivity.kt
+++ presentation/src/main/java/co/orange/presentation/auth/signup/SignUpActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseActivity
+import kr.genti.core.extension.hideKeyboard
+import kr.genti.core.extension.initFocusWithKeyboard
 import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivitySignUpBinding
@@ -17,6 +19,13 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sig
 
         binding.vm = viewModel
         initConfirmBtnListener()
+        initMobileBtnListener()
+        initVerifyBtnListener()
+        observeBirthFrontFinished()
+        observeBirthBackFinished()
+        observeMobileFinished()
+        observePhoneFinished()
+        observeCodeFinished()
     }
 
     private fun initConfirmBtnListener() {
@@ -25,6 +34,64 @@ class SignUpActivity : BaseActivity<ActivitySignUpBinding>(R.layout.activity_sig
 //                startActivity(this)
 //            }
 //            finish()
+        }
+    }
+
+    private fun initMobileBtnListener() {
+        binding.btnSelectMobile.setOnSingleClickListener { showMobileBottomSheet() }
+    }
+
+    private fun showMobileBottomSheet() {
+        // TODO
+    }
+
+    private fun initVerifyBtnListener() {
+        binding.btnConfirm.setOnSingleClickListener {
+            // TODO 서버통신
+        }
+    }
+
+    private fun observeBirthFrontFinished() {
+        viewModel.isBirthFrontFinished.observe(this) { isFinished ->
+            if (isFinished) {
+                binding.etSignUpBirthBack.requestFocus()
+            }
+        }
+    }
+
+    private fun observeBirthBackFinished() {
+        viewModel.isBirthBackFinished.observe(this) { isFinished ->
+            if (isFinished) {
+                currentFocus?.let { hideKeyboard(it) }
+                currentFocus?.clearFocus()
+                showMobileBottomSheet()
+            }
+        }
+    }
+
+    private fun observeMobileFinished() {
+        viewModel.isMobileFinished.observe(this) { isFinished ->
+            if (isFinished) {
+                initFocusWithKeyboard(binding.etSignUpPhone)
+            }
+        }
+    }
+
+    private fun observePhoneFinished() {
+        viewModel.isPhoneFinished.observe(this) { isFinished ->
+            if (isFinished) {
+                currentFocus?.let { hideKeyboard(it) }
+                currentFocus?.clearFocus()
+            }
+        }
+    }
+
+    private fun observeCodeFinished() {
+        viewModel.isCodeFinished.observe(this) { isFinished ->
+            if (isFinished) {
+                currentFocus?.let { hideKeyboard(it) }
+                currentFocus?.clearFocus()
+            }
         }
     }
 }

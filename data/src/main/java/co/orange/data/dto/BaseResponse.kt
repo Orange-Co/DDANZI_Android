b7ca501983599b94b66eb08837b85d4787a21c40
Sangho Kim <marchbreeze03@gmail.com>
2024-07-10 03:34:17 +0900

    [ADD/#37] BaseResopnse 추가

---
+++ data/src/main/java/co/orange/data/dto/BaseResponse.kt
@@ -0,0 +1,18 @@
+package co.orange.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponse<T>(
+    @SerialName("success")
+    val success: Boolean,
+    @SerialName("response")
+    val response: T,
+    @SerialName("code")
+    val code: String,
+    @SerialName("message")
+    val message: String,
+    @SerialName("status")
+    val status: Int,
+)

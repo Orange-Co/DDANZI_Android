fa5e49d1bea6001027150fbb025ea98f7a82d071
Sangho Kim <marchbreeze03@gmail.com>
2024-07-16 04:42:40 +0900

    [FEAT/#46] 내 관심목록 조회 API 구현

--- presentation/src/main/java/co/orange/presentation/setting/history/HistoryActivity.kt
+++ presentation/src/main/java/co/orange/presentation/setting/history/HistoryActivity.kt
@@ -4,9 +4,16 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import co.orange.core.base.BaseActivity
 import co.orange.core.extension.setOnSingleClickListener
 import co.orange.core.extension.stringOf
+import co.orange.core.extension.toast
+import co.orange.core.state.UiState
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityHistoryBinding
 
@@ -18,6 +25,7 @@ class HistoryActivity : BaseActivity<ActivityHistoryBinding>(R.layout.activity_h
 
         initBackBtnListener()
         getTypeFromIntent()
+        observeGetInterestListState()
     }
 
     private fun initBackBtnListener() {
@@ -41,10 +49,27 @@ class HistoryActivity : BaseActivity<ActivityHistoryBinding>(R.layout.activity_h
 
             TYPE_INTEREST -> {
                 binding.tvHistoryTitle.text = stringOf(R.string.profile_history_interest_title)
+                viewModel.getInterestListFromServer()
             }
 
             else -> return
         }
+    }
+
+    private fun observeGetInterestListState() {
+        viewModel.getInterestListState.flowWithLifecycle(lifecycle).distinctUntilChanged()
+            .onEach { state ->
+                when (state) {
+                    is UiState.Success -> {
+                        binding.tvHistoryCount.text =
+                            getString(R.string.profile_history_count, state.data.totalCount)
+                        // resultAdapter.addList(state.data.searchedProductList)
+                    }
+
+                    is UiState.Failure -> toast(stringOf(R.string.error_msg))
+                    else -> return@onEach
+                }
+            }.launchIn(lifecycleScope)
     }
 
     companion object {

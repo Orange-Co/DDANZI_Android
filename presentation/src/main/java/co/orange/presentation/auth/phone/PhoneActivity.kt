014a39faee1cb3fd0565ebd88a069397176b09e8
Sangho Kim <marchbreeze03@gmail.com>
2024-07-22 03:55:58 +0900

    [ADD/#50] 웹뷰 속성 추가

--- presentation/src/main/java/co/orange/presentation/auth/phone/PhoneActivity.kt
+++ presentation/src/main/java/co/orange/presentation/auth/phone/PhoneActivity.kt
@@ -1,6 +1,10 @@
 package co.orange.presentation.auth.phone
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.webkit.WebSettings
+import android.webkit.WebView
 import co.orange.core.base.BaseActivity
 import com.iamport.sdk.domain.core.Iamport
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,15 +28,30 @@ class PhoneActivity : BaseActivity<ActivityPhoneBinding>(R.layout.activity_phone
 //                ),
 //                "AndroidBridge",
 //            )
-            settings.javaScriptEnabled = true
-//            settings.javaScriptCanOpenWindowsAutomatically = true
-//            settings.cacheMode = WebSettings.LOAD_NO_CACHE
-//            setLayerType(View.LAYER_TYPE_HARDWARE, null)
-//            scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
-//            isScrollbarFadingEnabled = true
+            settings.apply {
+                javaScriptEnabled = true
+                javaScriptCanOpenWindowsAutomatically = true
+                cacheMode = WebSettings.LOAD_NO_CACHE
+            }
+            setLayerType(View.LAYER_TYPE_HARDWARE, null)
+            scrollBarStyle = WebView.SCROLLBARS_OUTSIDE_OVERLAY
+            isScrollbarFadingEnabled = true
 //            webChromeClient = myChromeClient()
 //            webViewClient = WebViewClientClass()
             loadUrl("file:///android_asset/web_auth_phone.html")
         }
+    }
+
+    override fun onKeyDown(
+        keyCode: Int,
+        event: KeyEvent?,
+    ): Boolean {
+        binding.webPhoneAuth.apply {
+            if ((keyCode == KeyEvent.KEYCODE_BACK) && this.canGoBack()) {
+                this.goBack()
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }

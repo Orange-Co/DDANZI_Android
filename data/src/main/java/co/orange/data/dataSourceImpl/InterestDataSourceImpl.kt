6c9bac24d869080ea602615e8d9995b3ffb070ab
Sangho Kim <marchbreeze03@gmail.com>
2024-07-27 01:51:51 +0900

    [FIX/#54] 좋아요 DataSource 연결 수정

--- data/src/main/java/co/orange/data/dataSourceImpl/InterestDataSourceImpl.kt
+++ data/src/main/java/co/orange/data/dataSourceImpl/InterestDataSourceImpl.kt
@@ -3,14 +3,15 @@ package co.orange.data.dataSourceImpl
 import co.orange.data.dataSource.InterestDataSource
 import co.orange.data.dto.BaseResponse
 import co.orange.data.dto.response.InterestDto
+import co.orange.data.service.InterestService
 import javax.inject.Inject
 
 data class InterestDataSourceImpl
     @Inject
     constructor(
-        private val interestDataSource: InterestDataSource,
+        private val interestService: InterestService,
     ) : InterestDataSource {
-        override suspend fun postInterest(productId: String): BaseResponse<InterestDto> = interestDataSource.postInterest(productId)
+        override suspend fun postInterest(productId: String): BaseResponse<InterestDto> = interestService.postInterest(productId)
 
-        override suspend fun deleteInterest(productId: String): BaseResponse<InterestDto> = interestDataSource.deleteInterest(productId)
+        override suspend fun deleteInterest(productId: String): BaseResponse<InterestDto> = interestService.deleteInterest(productId)
     }

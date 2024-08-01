d6e39c15376992ff67a99b02231a072699c5dbd8
Sangho Kim <marchbreeze03@gmail.com>
2024-08-02 03:51:13 +0900

    [ADD/#56] merchantUid local property 설정

--- presentation/src/main/java/co/orange/presentation/auth/phone/PhoneActivity.kt
+++ presentation/src/main/java/co/orange/presentation/auth/phone/PhoneActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import co.orange.core.base.BaseActivity
 import co.orange.core.extension.setOnSingleClickListener
+import co.orange.core.extension.stringOf
 import com.google.gson.GsonBuilder
 import com.iamport.sdk.data.sdk.IamPortCertification
 import com.iamport.sdk.data.sdk.IamPortResponse
 import com.iamport.sdk.domain.core.ICallbackPaymentResult
 import com.iamport.sdk.domain.core.Iamport
 import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.presentation.BuildConfig.MERCHANT_UID
+import kr.genti.presentation.BuildConfig.USER_CODE
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityPhoneBinding
 import timber.log.Timber
@@ -19,19 +22,18 @@ class PhoneActivity : BaseActivity<ActivityPhoneBinding>(R.layout.activity_phone
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Iamport.init(this)
         initAuthBtnListener()
     }
 
     private fun initAuthBtnListener() {
         binding.btnPhoneAuth.setOnSingleClickListener {
-            Iamport.init(this)
-            val userCode = "imp24882465"
+            val userCode = USER_CODE
             val certification =
                 IamPortCertification(
-                    merchant_uid = "MIIddanzi1",
-                    company = "유어포트",
+                    merchant_uid = MERCHANT_UID,
+                    company = stringOf(R.string.app_name),
                 )
-
             Iamport.certification(
                 userCode,
                 iamPortCertification = certification,
@@ -48,4 +50,9 @@ class PhoneActivity : BaseActivity<ActivityPhoneBinding>(R.layout.activity_phone
                 Timber.tag("qqqq").d(result.toString())
             }
         }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Iamport.close()
+    }
 }

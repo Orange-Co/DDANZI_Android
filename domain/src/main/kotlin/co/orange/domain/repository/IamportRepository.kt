1227eae948db2f149d979504ac95fe77150a2b9b
Sangho Kim <marchbreeze03@gmail.com>
2024-08-03 20:43:26 +0900

    [ADD/#56] Iamport 정보 조회 API domain 모듈 설정

--- domain/src/main/kotlin/co/orange/domain/repository/IamportRepository.kt
+++ domain/src/main/kotlin/co/orange/domain/repository/IamportRepository.kt
@@ -1,7 +1,10 @@
 package co.orange.domain.repository
 
+import co.orange.domain.entity.response.IamportCertificationModel
 import co.orange.domain.entity.response.IamportTokenModel
 
 interface IamportRepository {
     suspend fun postToGetIamportToken(): Result<IamportTokenModel>
+
+    suspend fun postToGetCertificationData(): Result<IamportCertificationModel>
 }

ac054205b6870791ddf5c152b3f1f414f2f52a22
Sangho Kim <marchbreeze03@gmail.com>
2024-08-07 04:00:42 +0900

    [FIX/#60] 주소검색 웹뷰 실행 지점 변경

--- presentation/src/main/java/co/orange/presentation/address/AddressActivity.kt
+++ presentation/src/main/java/co/orange/presentation/address/AddressActivity.kt
@@ -2,13 +2,40 @@ package co.orange.presentation.address
 
 import android.os.Bundle
 import co.orange.core.base.BaseActivity
+import co.orange.core.extension.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityAddressBinding
 
 @AndroidEntryPoint
 class AddressActivity : BaseActivity<ActivityAddressBinding>(R.layout.activity_address) {
+    override fun onStart() {
+        super.onStart()
+        AddressWebActivity.register(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initAddressFindBtnListener()
+    }
+
+    private fun initAddressFindBtnListener() {
+        with(binding) {
+            btnFindZipcode.setOnSingleClickListener { navigateToAddressWebView() }
+            btnFindAddress.setOnSingleClickListener { navigateToAddressWebView() }
+        }
+    }
+
+    private fun navigateToAddressWebView() {
+        AddressWebActivity.open { bundle ->
+            val zipCode = bundle.getString(AddressWebBridge.ZIPCODE)
+            val address = bundle.getString(AddressWebBridge.ADDRESS)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        AddressWebActivity.unregister()
     }
 }

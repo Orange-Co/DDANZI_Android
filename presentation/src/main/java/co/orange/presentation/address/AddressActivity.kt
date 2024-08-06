81e617901e58b9f0de6b3c82dbe2ffa65fc68812
Sangho Kim <marchbreeze03@gmail.com>
2024-08-07 03:55:14 +0900

    [ADD/#60] manifest에 주소지 등록뷰 추가

---
+++ presentation/src/main/java/co/orange/presentation/address/AddressActivity.kt
@@ -0,0 +1,14 @@
+package co.orange.presentation.address
+
+import android.os.Bundle
+import co.orange.core.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivityAddressBinding
+
+@AndroidEntryPoint
+class AddressActivity : BaseActivity<ActivityAddressBinding>(R.layout.activity_address) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

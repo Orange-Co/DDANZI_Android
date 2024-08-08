4bf6ae6207c90ee5647509adf8ee15d55567c3ff
Sangho Kim <marchbreeze03@gmail.com>
2024-08-08 17:59:38 +0900

    [ADD/#60] 주소지 추가, 조회 API domain 모듈 설정

---
+++ domain/src/main/kotlin/co/orange/domain/entity/response/AddressModel.kt
@@ -0,0 +1,11 @@
+package co.orange.domain.entity.response
+
+data class AddressModel(
+    val addressId: Long,
+    val name: String,
+    val zipCode: String,
+    val type: String,
+    val address: String,
+    val detailAddress: String,
+    val phone: String,
+)

e8320e128a87332f6d7ab6750911c7f59b481406
Sangho Kim <marchbreeze03@gmail.com>
2024-08-14 02:18:09 +0900

    [ADD/#70] 본인인증 완료 API domain 모듈 설정

--- domain/src/main/kotlin/co/orange/domain/repository/AuthRepository.kt
+++ domain/src/main/kotlin/co/orange/domain/repository/AuthRepository.kt
@@ -2,11 +2,18 @@ package co.orange.domain.repository
 
 import co.orange.domain.entity.request.AuthRequestModel
 import co.orange.domain.entity.request.ReissueRequestModel
+import co.orange.domain.entity.request.SignUpRequestModel
 import co.orange.domain.entity.response.AuthTokenModel
 import co.orange.domain.entity.response.ReissueTokenModel
+import co.orange.domain.entity.response.SignUpModel
 
 interface AuthRepository {
     suspend fun postReissueTokens(request: ReissueRequestModel): Result<ReissueTokenModel>
 
     suspend fun postOauthDataToGetToken(request: AuthRequestModel): Result<AuthTokenModel>
+
+    suspend fun postToSignUp(
+        authorization: String,
+        request: SignUpRequestModel,
+    ): Result<SignUpModel>
 }

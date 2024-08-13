fb0e69d2550921f3790c5d27309a65305f257afb
Sangho Kim <marchbreeze03@gmail.com>
2024-08-13 18:27:37 +0900

    [FEAT/#68] 카카오 로그인 이동 구현

--- presentation/src/main/java/co/orange/presentation/auth/login/LoginActivity.kt
+++ presentation/src/main/java/co/orange/presentation/auth/login/LoginActivity.kt
@@ -3,10 +3,18 @@ package co.orange.presentation.auth.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import co.orange.core.base.BaseActivity
 import co.orange.core.extension.setOnSingleClickListener
+import co.orange.core.extension.stringOf
+import co.orange.core.extension.toast
+import co.orange.core.state.UiState
 import co.orange.presentation.auth.phone.PhoneActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.ActivityLoginBinding
 
@@ -18,14 +26,36 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         super.onCreate(savedInstanceState)
 
         initLoginBtnListener()
+        observeAppLoginAvailable()
+        observeChangeTokenState()
     }
 
     private fun initLoginBtnListener() {
         binding.btnLoginKakao.setOnSingleClickListener {
-            Intent(this, PhoneActivity::class.java).apply {
-                startActivity(this)
-            }
-            finish()
+            viewModel.startLogInWithKakao(this)
         }
+    }
+
+    private fun observeAppLoginAvailable() {
+        viewModel.isAppLoginAvailable.flowWithLifecycle(lifecycle).onEach { isAvailable ->
+            if (!isAvailable) viewModel.startLogInWithKakao(this)
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun observeChangeTokenState() {
+        viewModel.changeTokenState.flowWithLifecycle(lifecycle).distinctUntilChanged()
+            .onEach { state ->
+                when (state) {
+                    is UiState.Success -> {
+                        Intent(this, PhoneActivity::class.java).apply {
+                            startActivity(this)
+                        }
+                        finish()
+                    }
+
+                    is UiState.Failure -> toast(stringOf(R.string.error_msg))
+                    else -> return@onEach
+                }
+            }.launchIn(lifecycleScope)
     }
 }
--- presentation/src/main/java/co/orange/presentation/auth/login/LoginViewModel.kt
+++ presentation/src/main/java/co/orange/presentation/auth/login/LoginViewModel.kt
@@ -1,12 +1,81 @@
 package co.orange.presentation.auth.login
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.orange.core.state.UiState
+import co.orange.domain.repository.AuthRepository
+import co.orange.domain.repository.UserRepository
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel
     @Inject
     constructor(
-        // private val authRepository: AuthRepository,
-    ) : ViewModel()
+        private val authRepository: AuthRepository,
+        private val userRepository: UserRepository,
+    ) : ViewModel() {
+        private val _isAppLoginAvailable = MutableStateFlow(true)
+        val isAppLoginAvailable: StateFlow<Boolean> = _isAppLoginAvailable
+
+        private val _changeTokenState = MutableStateFlow<UiState<String>>(UiState.Empty)
+        val changeTokenState: StateFlow<UiState<String>> = _changeTokenState
+
+        private var appLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                if (!(error is ClientError && error.reason == ClientErrorCause.Cancelled)) {
+                    _isAppLoginAvailable.value = false
+                }
+            } else if (token != null) {
+                changeTokenFromServer(token.accessToken)
+            }
+        }
+
+        private var webLoginCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error == null && token != null) {
+                changeTokenFromServer(token.accessToken)
+            }
+        }
+
+        fun startLogInWithKakao(context: Context) {
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context) && isAppLoginAvailable.value) {
+                UserApiClient.instance.loginWithKakaoTalk(
+                    context = context,
+                    callback = appLoginCallback,
+                )
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(
+                    context = context,
+                    callback = webLoginCallback,
+                )
+            }
+        }
+
+        private fun changeTokenFromServer(accessToken: String) {
+            viewModelScope.launch {
+                authRepository.postOauthDataToGetToken(AuthRequestModel(accessToken, KAKAO))
+                    .onSuccess {
+                        with(userRepository) {
+                            setTokens(it.accessToken, it.refreshToken)
+                            setUserRole(it.userRoleString)
+                        }
+                        _changeTokenState.value = UiState.Success(it.userRoleString)
+                    }
+                    .onFailure {
+                        _changeTokenState.value = UiState.Failure(it.message.toString())
+                    }
+            }
+        }
+
+        companion object {
+            const val KAKAO = "KAKAO"
+        }
+    }

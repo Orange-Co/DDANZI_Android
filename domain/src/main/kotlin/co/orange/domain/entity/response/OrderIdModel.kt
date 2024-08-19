0dc09cda42e1962a1d18856472e0171f6eb95bb8
Sangho Kim <marchbreeze03@gmail.com>
2024-08-20 05:21:15 +0900

    [ADD/#80] 구매 실행 API domain 모듈 설정

---
+++ domain/src/main/kotlin/co/orange/domain/entity/response/OrderIdModel.kt
@@ -0,0 +1,5 @@
+package co.orange.domain.entity.response
+
+data class OrderIdModel(
+    val orderId: String,
+)
--- domain/src/main/kotlin/co/orange/domain/repository/BuyRepository.kt
+++ domain/src/main/kotlin/co/orange/domain/repository/BuyRepository.kt
@@ -1,8 +1,10 @@
 package co.orange.domain.repository
 
+import co.orange.domain.entity.request.OrderRequestModel
 import co.orange.domain.entity.request.PayEndRequestModel
 import co.orange.domain.entity.request.PayStartRequestModel
 import co.orange.domain.entity.response.BuyProgressModel
+import co.orange.domain.entity.response.OrderIdModel
 import co.orange.domain.entity.response.PayEndModel
 import co.orange.domain.entity.response.PayStartModel
 
@@ -12,4 +14,6 @@ interface BuyRepository {
     suspend fun postPaymentStart(request: PayStartRequestModel): Result<PayStartModel>
 
     suspend fun patchPaymentEnd(request: PayEndRequestModel): Result<PayEndModel>
+
+    suspend fun postToRequestOrder(request: OrderRequestModel): Result<OrderIdModel>
 }

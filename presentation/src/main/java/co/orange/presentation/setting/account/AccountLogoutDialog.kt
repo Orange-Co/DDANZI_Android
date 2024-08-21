7f98441bb9698d94b8a44892be3b0c48cc5a93ea
Sangho Kim <marchbreeze03@gmail.com>
2024-08-22 01:24:15 +0900

    [ADD/#88] 로그아웃 다이얼로그 구현

---
+++ presentation/src/main/java/co/orange/presentation/setting/account/AccountLogoutDialog.kt
@@ -0,0 +1,68 @@
+package co.orange.presentation.setting.account
+
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import co.orange.core.base.BaseDialog
+import co.orange.core.extension.setOnSingleClickListener
+import co.orange.core.extension.stringOf
+import co.orange.core.extension.toast
+import co.orange.core.state.UiState
+import co.orange.presentation.setting.SettingViewModel
+import kotlinx.coroutines.delay
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.DialogAccountLogoutBinding
+
+class AccountLogoutDialog :
+    BaseDialog<DialogAccountLogoutBinding>(R.layout.dialog_account_logout) {
+    private val viewModel by activityViewModels<SettingViewModel>()
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.apply {
+            setLayout(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+            )
+            setBackgroundDrawableResource(R.color.transparent)
+        }
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initReturnBtnListener()
+        initLogoutBtnListener()
+        observeUserLogoutState()
+    }
+
+    private fun initReturnBtnListener() {
+        binding.btnReturn.setOnSingleClickListener { dismiss() }
+    }
+
+    private fun initLogoutBtnListener() {
+        binding.btnLogout.setOnSingleClickListener {
+            viewModel.logoutFromServer()
+        }
+    }
+
+    private fun observeUserLogoutState() {
+        viewModel.userLogoutState.flowWithLifecycle(lifecycle).distinctUntilChanged()
+            .onEach { state ->
+                when (state) {
+                    is UiState.Success -> {
+                        delay(500)
+                        restartApp(binding.root.context, null)
+                    }
+
+                    is UiState.Failure -> toast(stringOf(R.string.error_msg))
+                    else -> return@onEach
+                }
+            }.launchIn(lifecycleScope)
+    }
+}

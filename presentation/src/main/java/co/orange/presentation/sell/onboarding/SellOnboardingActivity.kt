3f747387ddc2ffc43a8aa46280600eb950b7a67e
Sangho Kim <marchbreeze03@gmail.com>
2024-09-01 15:01:21 +0900

    [ADD/#98] 판매 온보딩뷰 manifest 추가

--- presentation/src/main/java/co/orange/presentation/sell/onboarding/SellOnboardingActivity.kt
+++ presentation/src/main/java/co/orange/presentation/sell/onboarding/SellOnboardingActivity.kt
@@ -1,2 +1,15 @@
 package co.orange.presentation.sell.onboarding
 
+import android.os.Bundle
+import co.orange.core.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kr.genti.presentation.R
+import kr.genti.presentation.databinding.ActivitySellOnboardingBinding
+
+@AndroidEntryPoint
+class SellOnboardingActivity :
+    BaseActivity<ActivitySellOnboardingBinding>(R.layout.activity_sell_onboarding) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- presentation/src/main/java/co/orange/presentation/sell/onboarding/SellOnboardingViewModel.kt
+++ presentation/src/main/java/co/orange/presentation/sell/onboarding/SellOnboardingViewModel.kt
@@ -1,1 +1,12 @@
 package co.orange.presentation.sell.onboarding
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SellOnboardingViewModel
+    @Inject
+    constructor(
+        // private val homeRepository: HomeRepository,
+    ) : ViewModel()

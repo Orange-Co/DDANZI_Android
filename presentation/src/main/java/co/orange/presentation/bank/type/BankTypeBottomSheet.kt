7717dab40abd8b5130d500c0f264931add7bb472
Sangho Kim <marchbreeze03@gmail.com>
2024-09-03 18:52:28 +0900

    [FEAT/#107] 은행 선택뷰 어댑터 설정

--- presentation/src/main/java/co/orange/presentation/bank/type/BankTypeBottomSheet.kt
+++ presentation/src/main/java/co/orange/presentation/bank/type/BankTypeBottomSheet.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import co.orange.core.base.BaseBottomSheet
+import co.orange.domain.enums.BankType
 import co.orange.presentation.bank.add.BankAddViewModel
 import kr.genti.presentation.R
 import kr.genti.presentation.databinding.BottomSheetBankTypeBinding
@@ -11,6 +12,10 @@ import kr.genti.presentation.databinding.BottomSheetBankTypeBinding
 class BankTypeBottomSheet :
     BaseBottomSheet<BottomSheetBankTypeBinding>(R.layout.bottom_sheet_bank_type) {
     private val viewModel by activityViewModels<BankAddViewModel>()
+
+    private var _adapter: BankTypeAdapter? = null
+    val adapter
+        get() = requireNotNull(_adapter) { getString(R.string.adapter_not_initialized_error_msg) }
 
     override fun onStart() {
         super.onStart()
@@ -22,5 +27,28 @@ class BankTypeBottomSheet :
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        initBankTypeAdapter()
+        setBankType()
+    }
+
+    private fun initBankTypeAdapter() {
+        _adapter =
+            BankTypeAdapter(
+                itemClick = ::initBankTypeClickListener,
+            )
+        binding.rvBankType.adapter = adapter
+    }
+
+    private fun initBankTypeClickListener(item: BankType) {
+        with(viewModel) {
+            bankName.value = item.code
+            isBankSelected.value = true
+        }
+        dismiss()
+    }
+
+    private fun setBankType() {
+        adapter.submitList(BankType.values().toList())
     }
 }

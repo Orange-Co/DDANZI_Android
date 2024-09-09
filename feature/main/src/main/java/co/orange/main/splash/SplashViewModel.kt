0c2d55f6e1b0feca4482712f3fd07c0fb706126e
Sangho Kim <marchbreeze03@gmail.com>
2024-09-09 22:27:15 +0900

    [FEAT/#127] 서버 점검 API 구현

--- feature/main/src/main/java/co/orange/main/splash/SplashViewModel.kt
+++ feature/main/src/main/java/co/orange/main/splash/SplashViewModel.kt
@@ -1,2 +1,36 @@
 package co.orange.main.splash
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import co.orange.domain.repository.AuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel
+    @Inject
+    constructor(
+        private val authRepository: AuthRepository,
+    ) : ViewModel() {
+        private val _isServerAvailable = MutableSharedFlow<Boolean>()
+        val isServerAvailable: SharedFlow<Boolean> = _isServerAvailable
+
+        fun getServerStatusToCheckAvailable() {
+            viewModelScope.launch {
+                authRepository.getServerStatus()
+                    .onSuccess { isAvailable ->
+                        if (isAvailable) {
+                            _isServerAvailable.emit(true)
+                        } else {
+                            _isServerAvailable.emit(false)
+                        }
+                    }
+                    .onFailure {
+                        _isServerAvailable.emit(false)
+                    }
+            }
+        }
+    }

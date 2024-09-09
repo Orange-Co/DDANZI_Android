fe752ad21cce3ff1d05673526b0df3928c3891fb
Sangho Kim <marchbreeze03@gmail.com>
2024-09-09 16:08:51 +0900

    [ADD/#125] 알람 어댑터 추가

--- feature/main/src/main/java/co/orange/main/alarm/AlarmActivity.kt
+++ feature/main/src/main/java/co/orange/main/alarm/AlarmActivity.kt
@@ -1,2 +1,53 @@
 package co.orange.main.alarm
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import co.orange.core.base.BaseActivity
+import co.orange.core.extension.setOnSingleClickListener
+import co.orange.core.navigation.NavigationManager
+import co.orange.domain.entity.response.AlarmItemModel
+import co.orange.main.R
+import co.orange.main.databinding.ActivityAlarmBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class AlarmActivity : BaseActivity<ActivityAlarmBinding>(R.layout.activity_alarm) {
+    @Inject
+    lateinit var navigationManager: NavigationManager
+
+    private val viewModel by viewModels<AlarmViewModel>()
+
+    private var _adapter: AlarmListAdapter? = null
+    val adapter
+        get() = requireNotNull(_adapter) { getString(co.orange.core.R.string.adapter_not_initialized_error_msg) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initBackBtnListener()
+        initAdapter()
+    }
+
+    private fun initBackBtnListener() {
+        binding.btnBack.setOnSingleClickListener { finish() }
+    }
+
+    private fun initAdapter() {
+        _adapter =
+            AlarmListAdapter(
+                itemClick = ::initItemClickListener,
+            )
+        binding.rvAlarm.adapter = adapter
+    }
+
+    private fun initItemClickListener(item: AlarmItemModel) {
+        // TODO
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        _adapter = null
+    }
+}
--- feature/main/src/main/java/co/orange/main/alarm/AlarmViewModel.kt
+++ feature/main/src/main/java/co/orange/main/alarm/AlarmViewModel.kt
@@ -1,2 +1,13 @@
 package co.orange.main.alarm
 
+import androidx.lifecycle.ViewModel
+import co.orange.domain.repository.HomeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class AlarmViewModel
+    @Inject
+    constructor(
+        private val homeRepository: HomeRepository,
+    ) : ViewModel()

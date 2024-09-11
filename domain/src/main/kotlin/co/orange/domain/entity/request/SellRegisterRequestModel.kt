d1e903d6e316eaa5648679b6676da938fec3a9de
Sangho Kim <marchbreeze03@gmail.com>
2024-09-12 02:56:10 +0900

    [FIX/#131] 판매 요청 dto 날짜 수정

--- domain/src/main/kotlin/co/orange/domain/entity/request/SellRegisterRequestModel.kt
+++ domain/src/main/kotlin/co/orange/domain/entity/request/SellRegisterRequestModel.kt
@@ -3,6 +3,6 @@ package co.orange.domain.entity.request
 data class SellRegisterRequestModel(
     val productId: String,
     val productName: String,
-    val dueDate: String,
+    val receivedDate: String,
     val registeredImage: String,
 )
